5289c9759b8e619f9837129d4c2121b160de4400
hanhatlinh <hanhatlinh26@gmail.com>
2021-11-18 22:33:02 +0700

    add base chat

--- index.js
+++ index.js
@@ -41,8 +41,42 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => app.listen(PORT, () => console.log('Server running on PORT: ', PORT)))
-    .catch((error) => console.log(error.message));
 
+mongoose.connection.on("connected", () => {
+    console.log("Connected to DB")
+})
+
+mongoose.connection.on("error", (err) => {
+    console.log("error", err)
+})
 
 mongoose.set('useFindAndModify', false);
+
+const server = app.listen(PORT, () => console.log('Server running on PORT:',PORT))
+
+// Chat
+const io = require("socket.io")(server)
+
+io.of('/chat').on("connection", async(socket)=>{
+    console.log("connection " + socket.id)
+    
+    socket.broadcast.emit('test2',"hello "+socket.id)
+
+    socket.on("disconnect", ()=>{
+        console.log("disconnect "+socket.id)
+    })
+
+    socket.on("send-server", (msg,id) =>{
+        console.log({msg,id})
+        socket.join(id)
+        io.emit('send-client',msg)
+    })
+
+    // socket.on("send-server",async msg=>{
+    //     const iot = await io.fetchSockets()
+    //     console.log(msg)
+    //     console.dir(iot,{depth:null})
+    //     socket.broadcast.emit('test1',msg)
+    //     socket.emit('test1', msg)
+    // })
+})
